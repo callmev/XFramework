3e0c76bccc8f36b1660612df764135df5ce08e55
何国强 <heshuisheng@outlook.com>
2017-02-18 16:31:23 +0800

    增加AutoMapper单元测试

---
+++ XFramework.Test/Cases/AutoMapperTest .cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XFramework.Utilities;
+
+namespace XFramework.Test.Cases
+{
+    [TestClass]
+    public class AutoMapperTest
+    {
+        [TestMethod]
+        public void Map_Test()
+        {
+            var source = new MapSource()
+            {
+                UserId = "AA",
+                Name = "小明"
+            };
+
+            var obj = source.MapTo<MapObject>();
+
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(obj.Name, source.Name);
+            Assert.IsTrue(string.IsNullOrEmpty(obj.Id));
+        }
+    }
+
+    public class MapSource
+    {
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class MapObject
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
